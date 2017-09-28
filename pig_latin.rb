29586073082d6f766094eda5e4588d49bae719ca
c1505 <coreym247@gmail.com>
2017-09-28 20:57:31 +0000

    intial commit

---
+++ pig_latin.rb
@@ -0,0 +1,33 @@
+class PigLatin
+ VOWELS = %w(a e i o u)
+ CONSONANTS = ("a".."z").to_a - VOWELS
+ PREFIXES = %w(sch qu thr th ch yt xr)
+ def self.translate(word)
+   
+#   prefixes.each do |pre|
+#      if word.match? pre
+#       rest = word.slice((pre.length)..-1)
+#       return rest + "#{pre}ay"
+#      end
+#   end
+
+   if self.starts_with_vowels?(word)
+     word + "ay"
+   elsif self.starts_with_consonants?(word)
+     rest = word.slice!(1..-1)
+     rest + "#{word[0]}ay"
+   end
+ end
+
+ def self.starts_with_consonants?(word)
+   CONSONANTS.any? do |con|
+    word.start_with?(con)
+  end
+ end
+
+ def self.starts_with_vowels?(word)
+   VOWELS.any? do |vow|
+     word.start_with?(vow)
+   end
+ end
+end
