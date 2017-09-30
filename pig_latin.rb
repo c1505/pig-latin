66945ec8fbccd663339c644a9fd0a251da4cf0a2
c1505 <coreym247@gmail.com>
2017-09-30 19:16:37 +0000

    work in progress

--- pig_latin.rb
+++ pig_latin.rb
@@ -2,7 +2,8 @@ class PigLatin
   
   VOWELS = %w(a e i o u)
   CONSONANTS = ("a".."z").to_a - VOWELS
-  PREFIXES = %w(sch qu thr th ch yt xr)
+  CONSONANT_SOUND_GROUP = %w(ch)
+  VOWEL_SOUND_GROUP = []
  
   def self.translate(word)
    
@@ -13,32 +14,28 @@ class PigLatin
   #     end
   # end
   
-   if self.starts_with_prefix?(word)
-     rest = word.slice((pre.length)..-1)
-     rest + "#{pre}ay"
-   elsif self.starts_with_vowels?(word)
+
+   if self.vowel_sound?(word)
      word + "ay"
-   elsif self.starts_with_consonants?(word)
+   elsif self.consonant_sound?(word)
      rest = word.slice!(1..-1)
      rest + "#{word[0]}ay"
    end
   end
   
-  def self.starts_with_consonants?(word)
-    CONSONANTS.any? do |con|
+  def self.consonant_sound?(word)
+    arr = CONSONANT_SOUND_GROUP + CONSONANTS
+    arr.any? do |con|
       word.start_with?(con)
     end
   end
   
-  def self.starts_with_vowels?(word)
-   VOWELS.any? do |vow|
-     word.start_with?(vow)
-   end
-  end
-  
-  def self.starts_with_prefix?(word)
-   PREFIXES.any? do |pre|
+  def self.vowel_sound?(word)
+  arr = VOWELS + VOWEL_SOUND_GROUP
+   arr.any? do |pre|
      word.start_with?(pre)
    end
   end
+
+
 end
